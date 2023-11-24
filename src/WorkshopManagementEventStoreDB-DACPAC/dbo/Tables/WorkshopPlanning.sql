e5394338d3fe165639bfe475b54766d04978fb6d
twerthi <twerthi@hotmail.com>
2023-11-24 13:43:59 -0800

    Missed on DACPAC folder

---
+++ src/WorkshopManagementEventStoreDB-DACPAC/dbo/Tables/WorkshopPlanning.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[WorkshopPlanning] (
+    [Id]             VARCHAR (50) NOT NULL,
+    [CurrentVersion] INT          NOT NULL,
+    CONSTRAINT [PK__Workshop__3214EC07310129A6] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ src/WorkshopManagementEventStoreDB-DACPAC/dbo/Tables/WorkshopPlanningEvent.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[WorkshopPlanningEvent] (
+    [Id]          VARCHAR (50)  NOT NULL,
+    [Version]     INT           NOT NULL,
+    [Timestamp]   DATETIME2 (7) NOT NULL,
+    [MessageType] VARCHAR (75)  NOT NULL,
+    [EventData]   TEXT          NULL,
+    CONSTRAINT [PK__Workshop__72E1AC14FFF665F9] PRIMARY KEY CLUSTERED ([Id] ASC, [Version] ASC),
+    CONSTRAINT [FK__WorkshopPlan__Id__398D8EEE] FOREIGN KEY ([Id]) REFERENCES [dbo].[WorkshopPlanning] ([Id])
+);
+
